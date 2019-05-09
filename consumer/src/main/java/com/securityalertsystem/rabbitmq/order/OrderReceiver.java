1fd33c1c543dec6e4f2c3be613aae46cf7599c91
cyy <892471766@qq.com>
2019-05-08 19:09:38 -0700

    change directory

---
+++ consumer/src/main/java/com/securityalertsystem/rabbitmq/order/OrderReceiver.java
@@ -0,0 +1,35 @@
+package com.securityalertsystem.rabbitmq.order;
+
+import com.securityalertsystem.rabbitmq.entity.Order;
+import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import com.rabbitmq.client.Channel;
+
+import java.util.Map;
+
+@Component
+public class OrderReceiver {
+
+    @RabbitListener(
+            bindings = @QueueBinding(
+                    value = @Queue(value = "order-queue",durable="true"),
+                    exchange = @Exchange(name = "order-exchange",durable = "true",type="topic"),
+                    key = "order.*"
+            )
+    )
+    @RabbitHandler
+    public void onOrderMessage(@Payload Order order,
+                               @Headers Map<String,Object> headers,
+                               Channel channel) throws Exception{
+        //Consumer Operation
+        System.err.println("----------received message-----------");
+        System.err.println("order ID: "+order.getId());
+
+        Long deliveryTag = (Long)headers.get(AmqpHeaders.DELIVERY_TAG);
+        channel.basicAck(deliveryTag,false);
+    }
+
+}
